c3d3bb5393167ba6b80e4a9332246e6bad1fcb64
gustavo <gamendez98@gmail.com>
2024-06-06 15:41:11 -0400

    small changes

--- server.go
+++ server.go
@@ -1,27 +1,25 @@
 package main
 
 import (
-	tmw "gec-applet/templateMiddleware"
+	"gec-applet/templates"
 	"github.com/labstack/echo/v4"
-	"html/template"
 	"net/http"
 )
 
 func Hello(c echo.Context) error {
-	return c.Render(http.StatusOK, "response", "World")
+	templateResult := templates.Hello("world!!!")
+	c.Response().Header().Set(echo.HeaderContentType, echo.MIMETextHTML)
+
+	return templateResult.Render(c.Request().Context(), c.Response().Writer)
 }
 
 func main() {
-	t := &tmw.Template{
-		Templates: template.Must(template.ParseGlob("public/views/*.html")),
-	}
 
 	e := echo.New()
 
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.Renderer = t
 	e.GET("/hello", Hello)
 	e.Logger.Fatal(e.Start(":1323"))
 }
